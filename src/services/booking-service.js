e96aa4626cd5fc8723c1eb459b5c5d0cc2daf4a3
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-11-14 20:59:50 +0530

    we are able to communicate two services

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,1 +1,22 @@
-module.exports = {};
+const axios = require("axios");
+
+const { BookingRepository } = require("../repositories");
+
+const { ServerConfig } = require("../config");
+
+const db = require("../models");
+
+async function createBooking(data) {
+  try {
+    const flight = await axios.get(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
+    );
+    console.log(flight);
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { createBooking };
